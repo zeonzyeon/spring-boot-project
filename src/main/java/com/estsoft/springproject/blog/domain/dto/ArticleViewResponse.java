fe04afaeda65c004e727ca52cd3d435dff3c5574
Jihyun Jeon <hyuneejj@naver.com>
2024-10-17 13:36:14 +0900

    블로그 글 날짜 수정

--- src/main/java/com/estsoft/springproject/blog/domain/dto/ArticleViewResponse.java
+++ src/main/java/com/estsoft/springproject/blog/domain/dto/ArticleViewResponse.java
@@ -18,10 +18,13 @@ public class ArticleViewResponse {
     private String title;
     private String content;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public ArticleViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+        this.updatedAt = article.getUpdatedAt();
     }
 }
