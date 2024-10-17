668b61c3d82817fd1a94a039a1d8b5ba09614e9f
Jihyun Jeon <hyuneejj@naver.com>
2024-10-17 13:27:35 +0900

    블로그 글 삭제 기능 구현

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Sprint-Boot Blog
+# Spring-Boot Blog
 
 - `Spring Boot`로 API 구현
 - `Thymleaf`를 이용하여 Blog 화면 구성
+- `JavaScript`로 버튼 기능 구현
---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,14 @@
+// 삭제 기능 코드 작성
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            alert('삭제가 완료되었습니다');
+            location.replace('/articles');
+        });
+    });
+}
