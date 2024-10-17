f51cc5b0ef2e89c4841f9536995a4272643b95e9
Jihyun Jeon <hyuneejj@naver.com>
2024-10-17 14:34:14 +0900

    블로그 글 생성 기능 구현

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,4 +1,4 @@
-// 삭제 기능 코드 작성
+// 삭제 기능 구현
 const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
@@ -36,4 +36,25 @@ if (modifyButton) {
             location.replace(`/articles/${id}`);
         });
     });
+}
+
+// 생성 기능 구현
+const createButton = document.getElementById('create-btn');
+
+if (createButton) {
+    createButton.addEventListener('click', event => {
+        fetch(`/api/articles`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body : JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            }),
+        }).then(() => {
+            alert('등록 완료되었습니다');
+            location.replace("/articles");
+        })
+    })
 }
