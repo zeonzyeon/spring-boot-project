5fefcc5b1a390aca41df1516000ae5487d3478c8
Jihyun Jeon <hyuneejj@naver.com>
2024-11-19 13:46:28 +0900

    [테스트 주도 개발] 은행 입/출금 테스트 수정

--- src/test/java/com/estsoft/springproject/tdd/AccountTest.java
+++ src/test/java/com/estsoft/springproject/tdd/AccountTest.java
@@ -2,6 +2,8 @@ package com.estsoft.springproject.tdd;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // TDD
@@ -9,30 +11,33 @@ import org.junit.jupiter.api.Test;
 // 2. 잔금 조회 (v)
 // 3. 입/출금 (v)
 public class AccountTest {
+	Account account;
+
+	@BeforeEach
+	public void setUp() {
+		Account account = new Account(10000);
+	}
+
 	@Test
 	public void testAccount() {
-		Account account = new Account(10000);
 		assertThat(account.getBalance(), is(10000)); // hamcrest로 검증
 
-		Account account2 = new Account(20000);
-		assertThat(account2.getBalance(), is(20000));
+		account = new Account(20000);
+		assertThat(account.getBalance(), is(20000));
 
-		Account account3 = new Account(30000);
-		assertThat(account3.getBalance(), is(30000));
+		account = new Account(30000);
+		assertThat(account.getBalance(), is(30000));
 	}
 
 	@Test
 	public void testDeposit() {
-		Account account = new Account(10000);
 		account.deposit(100000);
 		assertThat(account.getBalance(), is(110000)); // 입금 기능 검증 완료
 	}
 
 	@Test
 	public void testWithdraw() {
-		Account account = new Account(10000);
 		account.withdraw(10000);
 		assertThat(account.getBalance(), is(0));
-
 	}
 }
